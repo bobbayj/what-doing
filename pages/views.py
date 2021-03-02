654c31ec26f17f089ebad1fc1c65668b86b3f2e4
BobbayJ <bobjin@Bob-Windows.localdomain>
2021-03-03 00:04:14 +1100

    update app and start afresh

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hey world, we at the polls index! And fuck!")
+    return HttpResponse("<h1>Start of something big</h1>")
