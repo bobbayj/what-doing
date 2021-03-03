f4b8abd3292208bf83d1e37c26e3fcbc79beb5c4
BobbayJ <bobjin@Bob-Windows.localdomain>
2021-03-03 20:18:18 +1100

    Start afresh with a blog app

--- whatdoing/settings.py
+++ whatdoing/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    # 'blog.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- whatdoing/urls.py
+++ whatdoing/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    # path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
